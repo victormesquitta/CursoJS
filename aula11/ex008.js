53f71cdaff2896e474903da8c5c48755ac229afe
Victor Mesquitta <mvictormesquita@gmail.com>
2022-08-23 00:00:20 -0300

    Aprendendo sobre condições em JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80.6
+console.log(`A velocidade do seu carro é de ${vel}km/h.`)
+if (vel > 80){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
