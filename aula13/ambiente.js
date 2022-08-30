d91fb4bd2a2694e8f67234ed9a288ee2e06762b7
Victor Mesquitta <mvictormesquita@gmail.com>
2022-08-29 23:42:02 -0300

    Estruturas de repetição e Depuração/Debug

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+// Utilizando o While - Verifica a condição antes de executar o bloco
+
+var c = 1
+while (c <= 6){
+    console.log(`${c}ª Posição.`)
+    c++
+}
+
+
+// Utilizando o Do-While - Executa o bloco e verifica a condição ao final
+
+var c = 1
+do {
+    console.log(`${c}ª Posição`)
+    c++
+
+} while (c <= 6)
---
+++ aula14/ambiente.js
@@ -0,0 +1,13 @@
+// Uilizando o For - Tem início, fim e incremento embutido
+
+for (var c = 1; c <= 6; c++){
+    console.log(`${c}ª Posição.`)
+}
+
+// Depuração | Debug
+
+console.log("Inicialiando...")
+for (var c = 1; c <= 6; c++){
+    console.log(`${c}ª Posição.`)
+}
+console.log("FIM DA EXECUÇÃO...")
