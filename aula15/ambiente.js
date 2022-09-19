afca1615e8c6092486e3f8abcfe7d41d8f8e2b0a
Victor Mesquitta <mvictormesquita@gmail.com>
2022-09-18 23:26:23 -0300

    Aula sobre Vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,14 @@
+let numeros = [2, 8, 7, 13, 5]
+/*
+numeros.push(20)
+console.log(`Nosso vetor é o ${numeros}.`)
+console.log(`O vetor tem ${numeros.length} posições.`)
+console.log(`O primeiro valor do vetor é ${numeros[0]}.`)
+
+
+for(let pos in numeros){
+    console.log(`A posição ${pos} tem o elemento ${numeros[pos]}.`)
+}
+*/
+
+console.log(numeros.indexOf(5))
